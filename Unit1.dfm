2b50b9da9a5e0ae977694fa42e1c9ab27126883f
kadin_lee <531662161@qq.com>
2020-03-30 08:19:39 +0800

    初步完成提交
    功能完成提交

---
+++ Unit1.dfm
@@ -0,0 +1,44 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = #19977#22269#39033#30446#22270#38598#35299#21253#24037#20855
+  ClientHeight = 520
+  ClientWidth = 781
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  DesignSize = (
+    781
+    520)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lbl1: TLabel
+    Left = 8
+    Top = 8
+    Width = 765
+    Height = 105
+    Caption = 
+      #35831#25302#25918'png'#22270#38598#25991#20214#25110#32773'json'#25991#20214#21040#31383#20307#19978#65292#36827#34892#35299#21253#13#10#25903#25345#25302#25918#22810#20010#25991#20214#12290#13#10#35299#21253#30340#22270#38598#25991#20214#23558#23384#25918#22312#22270#38598#30446#24405#30340': "'#21407#22987#25991#20214#21517 +
+      '_unpacked" '#30446#24405#19979
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object mmo_log: TMemo
+    Left = 8
+    Top = 128
+    Width = 765
+    Height = 384
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    ScrollBars = ssBoth
+    TabOrder = 0
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,207 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,Winapi.ShellAPI,QJson,Vcl.Imaging.pngimage;
+
+type
+  TForm1 = class(TForm)
+    mmo_log: TMemo;
+    lbl1: TLabel;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    procedure WmDropFiles(var Msg: TMessage); message WM_DROPFILES;
+    procedure DoUnPack(FileName:String);
+  end;
+
+
+  TChunkIHDRHelper = class helper for TChunkIHDR
+  public
+    function GetPerPixelByteSize():Integer;
+
+  end;
+
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+{ TForm1 }
+
+procedure TForm1.DoUnPack(FileName: String);
+var
+  jsonFileName:String;
+  Json,frames,node : TQJson;
+  i:Integer;
+  Png : TPngImage;
+  tempPng : TPngImage;
+  W,H,X,Y:Integer;
+  targetPath : String;
+  AX,AY:Integer;
+  pPngAlphaLine:PByte;
+  pTempPngAlphaLine:PByte;
+  PerPixelSize : Integer;
+  ALineLen : Integer;
+begin
+  mmo_log.Lines.Add('开始处理:' + FileName);
+  FileName := ChangeFileExt(FileName,'.png');
+  if not FileExists(FileName) then
+  begin
+    mmo_log.Lines.Add('图集文件不存在:' + FileName);
+    Exit;
+  end;
+
+  jsonFileName := ChangeFileExt(FileName,'.json');
+  if not FileExists(jsonFileName) then
+  begin
+    mmo_log.Lines.Add('json描述文件不存在:' + jsonFileName);
+    Exit;
+  end;
+
+  Json := TQJson.Create;
+  json.LoadFromFile(jsonFileName);
+  frames := Json.ItemByPath('.frames');
+
+  if frames = nil then
+  begin
+    mmo_log.Lines.Add('无法找到 frames 子节点');
+    Exit;
+  end;
+
+  targetPath := ChangeFileExt(FileName,'') + '_unpacked';
+
+  if not DirectoryExists(targetPath) then
+  begin
+    if not ForceDirectories(targetPath) then
+    begin
+      raise Exception.Create('无法创建目录:' + targetPath );
+    end;
+  end;
+
+  Png := TPngImage.Create;
+  Png.LoadFromFile(FileName);
+
+//  for i := 0 to Png.Chunks.Count - 1 do
+//  begin
+//    mmo_log.Lines.Add('Chk: Png - ' + I .ToString + ':' + Png.Chunks.Item[i].Name);
+//  end;
+
+  PerPixelSize := Png.Header.GetPerPixelByteSize();
+
+  for i := 0 to frames.Count - 1 do
+  begin
+    node :=  frames.Items[i];
+
+    W := Node.IntByName('w',0);
+    h := Node.IntByName('h',0);
+    x := Node.IntByName('x',0);
+    y := Node.IntByName('y',0);
+
+    if (W <=0) or (H <=0) then
+    begin
+      mmo_log.Lines.Add('跳过 宽度高度 为0的图片:' + Node.Name);
+      Continue;
+    end;
+
+    tempPng := TPngImage.Create;
+    tempPng.CreateBlank(Png.Header.ColorType,png.header.BitDepth,w,h);
+
+    ALineLen := TempPng.Width * PerPixelSize;
+
+    //先直接拷贝像素数据
+    for AY := 0 to tempPng.Height - 1 do
+    begin
+      pPngAlphaLine := PByte(Png.Scanline[AY + Y]);
+      pTempPngAlphaLine := PByte(tempPng.Scanline[AY]);
+      Inc(pPngAlphaLine,X * PerPixelSize);
+      Move(pPngAlphaLine^,pTempPngAlphaLine^,ALineLen);
+    end;
+
+
+    //然后处理透明通道的数据
+    if Png.Header.ColorType = COLOR_PALETTE  then
+    begin
+      tempPng.Palette := Png.Palette;
+
+      //如果是调色板模式那么直接拷贝透明通道数据
+      if Png.TransparencyMode <> ptmNone then
+      begin
+        tempPng.CreateAlpha();
+        tempPng.Chunks.ItemFromClass(TChunktRNS).Assign(Png.Chunks.ItemFromClass(TChunktRNS));
+      end;
+    end else
+    begin
+      if Png.TransparencyMode <> ptmNone then
+      begin
+        tempPng.CreateAlpha();
+        for AY := 0 to tempPng.Height - 1 do
+        begin
+          pPngAlphaLine := PByte(Png.AlphaScanline[AY + Y]);
+          pTempPngAlphaLine := PByte(tempPng.AlphaScanline[AY]);
+          Inc(pPngAlphaLine,X);
+          Move(pPngAlphaLine^,pTempPngAlphaLine^,tempPng.Width);
+        end;
+      end;
+    end;
+
+//    for AX := 0 to tempPng.Chunks.Count - 1 do
+//    begin
+//      mmo_log.Lines.Add('Chk: TempPng - ' + AX .ToString + ':' + tempPng.Chunks.Item[AX].Name);
+//    end;
+
+    tempPng.SaveToFile(targetPath + '\' + node.Name + '.png');
+    tempPng.Free;
+  end;
+
+  png.Free;
+  Json.Free;
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  ChangeWindowMessageFilter(WM_DROPFILES, MSGFLT_ADD);
+
+  ChangeWindowMessageFilter(WM_COPYDATA, MSGFLT_ADD);
+
+  ChangeWindowMessageFilter(WM_COPYGLOBALDATA , MSGFLT_ADD);
+
+   DragAcceptFiles(Form1.Handle, True);
+end;
+
+procedure TForm1.WmDropFiles(var Msg: TMessage);
+var
+   P:array[0..511] of Char;
+   i:Word;
+begin
+   Inherited;
+   {$IFDEF WIN32}
+      i:=DragQueryFile(Msg.wParam,$FFFFFFFF,nil,0);
+   {$ELSE}
+      i:=DragQueryFile(Msg.wParam,$FFFF,nil,0);
+   {$ENDIF}
+   for i:=0 to i-1 do
+   begin
+     DragQueryFile(Msg.wParam,i,P,512);
+     DoUnPack(StrPas(P));
+   end;
+
+   mmo_log.Lines.Add('所有文件处理完成');
+end;
+
+{ TChunkIHDRHelper }
+
+function TChunkIHDRHelper.GetPerPixelByteSize: Integer;
+begin
+  Result := BytesPerRow div Width;
+end;
+
+end.
